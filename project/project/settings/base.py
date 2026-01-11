83aca11d9ccff080b251b10abe2fc5445c2da4da
a-ilawy <mahmed20156947@gmail.com>
2026-01-11 23:25:53 +0200

    feat(settings): add test settings env

--- project/project/settings/base.py
+++ project/project/settings/base.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-dev-key")
 SECRET_KEY = config('SECRET_KEY', default='unsafe-default-key')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv() , default='*')
 
 
 AUTH_USER_MODEL = "accounts.User"
